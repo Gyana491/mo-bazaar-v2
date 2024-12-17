6396fe06c7e597f6e0bf7eb047c9998cd68b3bce
Gyana Ranjan <Gyanaranjanmohanta491@gmail.com>
2024-12-17 21:40:39 +0530

    removed phone number

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -7,7 +7,6 @@ const Form = () => {
         <input className='border border-gray-300 rounded-md p-2' type="text" placeholder="Name" />
         <input className='border border-gray-300 rounded-md p-2' type="email" placeholder="Email" />
         <input className='border border-gray-300 rounded-md p-2' type="password" placeholder="Password" />
-        <input className='border border-gray-300 rounded-md p-2' type="number" placeholder="Phone Number" />
         <button className='bg-blue-500 text-white rounded-md p-2' type="submit">Submit</button>
     </form>
     </>
