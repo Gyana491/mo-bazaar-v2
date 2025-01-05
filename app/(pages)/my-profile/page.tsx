a502ea4926487d62261ec898e0b7f8dc3b2f8bef
Gyana Ranjan <Gyanaranjanmohanta491@gmail.com>
2025-01-06 03:36:43 +0530

    Modified Big Changes

---
+++ app/(pages)/my-profile/page.tsx
@@ -0,0 +1,20 @@
+import Header from "@/app/components/Header";
+import MobileNavigation from "@/app/components/MobileNavigation";
+
+const MyProfile = () => {
+  return (
+    <>
+    <Header />
+    <MobileNavigation />
+    <div className="bg-white dark:bg-gray-900">
+      <div className="max-w-3xl px-4 py-4 mx-auto">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white text-center">
+          My Profile
+        </h2>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default MyProfile;
